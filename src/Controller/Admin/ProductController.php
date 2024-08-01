bd79697e48d41b71b8fd49283c5d141b416f8f7b
Edjek <edjek@hotmail.fr>
2024-08-01 14:32:22 +0200

    update

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 #[Route('/admin/produit', name: 'admin_product_')]
 class ProductController extends AbstractController
@@ -21,6 +22,14 @@ class ProductController extends AbstractController
 
         return $this->render('admin/product/index.html.twig', [
             'products' => $pagination
+        ]);
+    }
+
+    #[Route('/detail/{slug}', name: 'show', methods: ['GET'])]
+    public function show(?Product $product)
+    {
+        return $this->render('admin/product/show.html.twig', [
+            'product' => $product,
         ]);
     }
 
@@ -43,5 +52,34 @@ class ProductController extends AbstractController
         return $this->render('admin/product/new.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    #[Route('/modifier/{slug}', name: 'edit', methods: ['GET', 'POST'], requirements: ['slug' => Requirement::ASCII_SLUG])]
+    public function edit(?Product $product, EntityManagerInterface $em, Request $request): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Vous avez modifié un produit.');
+
+            return $this->redirectToRoute('admin_product_index');
+        }
+
+        return $this->render('admin/product/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/supprimer/{id}', name: 'delete', methods: ['DELETE'], requirements: ['id' => Requirement::DIGITS])]
+    public function delete(?Product $product, EntityManagerInterface $em)
+    {
+        $em->remove($product);
+        $em->flush();
+
+        $this->addFlash('danger', 'Vous venez de supprimer un produit!');
+        return $this->redirectToRoute('admin_product_index');
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -200,6 +200,12 @@ class Product
     {
         $this->imageFile = $imageFile;
 
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
         return $this;
     }
 }
